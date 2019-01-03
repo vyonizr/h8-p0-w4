517582f9396ef1948df0724ebbbc3a17a2a9f577
vyonizr <fitrahtur@gmail.com>
2019-01-03 18:42:27 +0700

    Added 'exercise-7.js'

---
+++ exercise-7.js
@@ -0,0 +1,16 @@
+function urutkanAbjad(str) {
+  // you can only write your code here!
+  let strArray = [];
+
+  for (let i = str.length - 1; i >= 0; i--) {
+      strArray.push(str[i]);
+  }
+  return strArray.sort().join("");
+}
+  
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
